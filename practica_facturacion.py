636eeeb9c0c759e2b15a81222e7dce4e57ce013a
monse1224 <A01709064@tec.mx>
2024-04-10 18:21:20 -0600

    practica facturacion 2

--- practica_facturacion.py
+++ practica_facturacion.py
@@ -6,3 +6,7 @@ print(df.head())
 filtro1=df[(df["CVE_CLPV"] >= 1000) & (df["CVE_CLPV"]<=2000)]
 print(filtro1)
 filtro1.to_csv('practica_facturacion_1.csv')
+
+filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
+print(filtro2)
+filtro2.to_csv('practica_facturacion_2.csv')    
